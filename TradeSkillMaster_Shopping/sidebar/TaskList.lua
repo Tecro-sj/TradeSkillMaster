8356080a70067b9fdc5613dbb1ecdfb7a61a8aea
Tecro-sj <joncerber@gmail.com>
2025-11-21 11:15:54 +0100

    Updated TaskList.lua

--- TradeSkillMaster_Shopping/sidebar/TaskList.lua
+++ TradeSkillMaster_Shopping/sidebar/TaskList.lua
@@ -21,7 +21,7 @@ function private.Create(parent)
 	local queueSTParent = CreateFrame("Frame", nil, frame)
 	queueSTParent:SetPoint("TOPLEFT", helpText, "BOTTOMLEFT", 0, -5)
 	queueSTParent:SetPoint("TOPRIGHT", helpText, "BOTTOMRIGHT", 0, -5)
-	queueSTParent:SetHeight(150)
+	queueSTParent:SetHeight(120)
 	TSMAPI.Design:SetFrameColor(queueSTParent)
 
 	local queueCols = {
@@ -33,11 +33,33 @@ function private.Create(parent)
 	local stHandlers = {
 		OnEnter = function(_, data, self)
 			if not data or not data.spellID then return end
+			local TSMCrafting = LibStub("AceAddon-3.0"):GetAddon("TSM_Crafting", true)
+			if not TSMCrafting or not TSMCrafting.db or not TSMCrafting.db.realm or not TSMCrafting.db.realm.crafts then return end
+
 			GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-			GameTooltip:AddLine(data.name)
+			GameTooltip:AddLine(data.name .. " (x" .. data.queued .. ")")
+
 			if data.profit then
-				GameTooltip:AddLine("Profit per craft: " .. TSMAPI:FormatTextMoney(data.profit))
+				local color = data.profit < 0 and "|cffff0000" or "|cff00ff00"
+				GameTooltip:AddLine("Profit: " .. TSMAPI:FormatTextMoney(data.profit, color))
+				if data.queued > 1 then
+					local totalProfit = data.profit * data.queued
+					GameTooltip:AddLine("Total Profit: " .. TSMAPI:FormatTextMoney(totalProfit, color))
+				end
 			end
+
+			local craft = TSMCrafting.db.realm.crafts[data.spellID]
+			if craft and craft.mats then
+				GameTooltip:AddLine(" ")
+				for itemString, matQuantity in pairs(craft.mats) do
+					local itemName = GetItemInfo(itemString) or itemString
+					local inventory = TSMCrafting.Inventory:GetPlayerBagNum(itemString) or 0
+					local need = matQuantity * data.queued
+					local color = inventory >= need and "|cff00ff00" or "|cffff0000"
+					GameTooltip:AddLine(color .. inventory .. "/" .. need .. "|r " .. itemName, 1, 1, 1)
+				end
+			end
+
 			GameTooltip:Show()
 		end,
 		OnLeave = function()
@@ -45,12 +67,22 @@ function private.Create(parent)
 		end
 	}
 
-	frame.queueST = TSMAPI:CreateScrollingTable(queueSTParent, queueCols, stHandlers, 10)
+	frame.queueST = TSMAPI:CreateScrollingTable(queueSTParent, queueCols, stHandlers, 8)
 	frame.queueST:DisableSelection(true)
 
+	local searchBtn = TSMAPI.GUI:CreateButton(frame, 18)
+	searchBtn:SetPoint("TOPLEFT", queueSTParent, "BOTTOMLEFT", 0, -5)
+	searchBtn:SetPoint("TOPRIGHT", queueSTParent, "BOTTOMRIGHT", 0, -5)
+	searchBtn:SetHeight(25)
+	searchBtn:SetText("Search Auction")
+	searchBtn:SetScript("OnClick", function()
+		private.SearchAuction()
+	end)
+	frame.searchBtn = searchBtn
+
 	local matsText = TSMAPI.GUI:CreateLabel(frame)
-	matsText:SetPoint("TOPLEFT", queueSTParent, "BOTTOMLEFT", 0, -10)
-	matsText:SetPoint("TOPRIGHT", queueSTParent, "BOTTOMRIGHT", 0, -10)
+	matsText:SetPoint("TOPLEFT", searchBtn, "BOTTOMLEFT", 0, -10)
+	matsText:SetPoint("TOPRIGHT", searchBtn, "BOTTOMRIGHT", 0, -10)
 	matsText:SetHeight(25)
 	matsText:SetJustifyH("CENTER")
 	matsText:SetJustifyV("CENTER")
@@ -78,14 +110,60 @@ function private.Create(parent)
 		end,
 		OnLeave = function()
 			GameTooltip:Hide()
+		end,
+		OnClick = function(_, data)
+			if data and data.itemString and IsAltKeyDown() then
+				local itemName = GetItemInfo(data.itemString)
+				if itemName then
+					TSM:GetModule("Search"):StartItemSearch(itemName)
+				end
+			end
 		end
 	}
 
-	frame.matsST = TSMAPI:CreateScrollingTable(matsSTParent, matsCols, matsHandlers, 8)
+	frame.matsST = TSMAPI:CreateScrollingTable(matsSTParent, matsCols, matsHandlers, 6)
 	frame.matsST:DisableSelection(true)
 
 	private.frame = frame
 	return frame
+end
+
+function private.SearchAuction()
+	local TSMCrafting = LibStub("AceAddon-3.0"):GetAddon("TSM_Crafting", true)
+	if not TSMCrafting or not TSMCrafting.db or not TSMCrafting.db.realm or not TSMCrafting.db.realm.crafts then
+		TSM:Print("No craft queue found.")
+		return
+	end
+
+	local materials = {}
+	for spellID, data in pairs(TSMCrafting.db.realm.crafts) do
+		if data.queued and data.queued > 0 and data.mats then
+			for itemString, quantity in pairs(data.mats) do
+				local totalNeeded = quantity * data.queued
+				local have = TSMCrafting.Inventory:GetPlayerBagNum(itemString) or 0
+				local need = max(totalNeeded - have, 0)
+				if need > 0 then
+					materials[itemString] = (materials[itemString] or 0) + need
+				end
+			end
+		end
+	end
+
+	local searchTerms = {}
+	for itemString in pairs(materials) do
+		local itemName = GetItemInfo(itemString)
+		if itemName then
+			tinsert(searchTerms, itemName)
+		end
+	end
+
+	if #searchTerms == 0 then
+		TSM:Print("All materials available!")
+		return
+	end
+
+	local searchString = table.concat(searchTerms, ";")
+	TSM:GetModule("Search"):StartItemSearch(searchString)
 end
 
 function private.UpdateTaskList()
@@ -122,6 +200,7 @@ function private.UpdateTaskList()
 				},
 				spellID = spellID,
 				name = data.name,
+				queued = data.queued,
 				profit = profit
 			})
 
@@ -136,18 +215,13 @@ function private.UpdateTaskList()
 	private.frame.queueST:SetData(queueData)
 
 	local matsData = {}
-	local Inventory = TSMCrafting:GetModule("Inventory", true)
 	for itemString, totalNeeded in pairs(matsNeeded) do
-		local itemName = TSMAPI.Item:GetName(itemString)
+		local itemName = GetItemInfo(itemString)
 		if not itemName then
 			itemName = itemString
 		end
 
-		local have = 0
-		if Inventory then
-			have = Inventory:GetTotalQuantity(itemString) or 0
-		end
-
+		local have = TSMCrafting.Inventory:GetPlayerBagNum(itemString) or 0
 		local need = max(totalNeeded - have, 0)
 		local needText = need > 0 and "|cffff0000" .. need .. "|r" or "|cff00ff00" .. need .. "|r"
 
